43a57a6be4bab70069aa0d34e95cb5b8bd968de9
Imran S <trade2cart@gmail.com>
2025-08-12 10:08:25 +0530

    Update Loader.jsx

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -4,30 +4,35 @@ import styled, { keyframes } from 'styled-components';
 
 // --- Animation Keyframes ---
 
-// Keyframes for the items flying into the cart
-const addToCartAnimation = keyframes`
-  0% {
-    opacity: 0;
-    transform: translate(0, 0) scale(1);
+// Keyframes for the scanning line effect
+const scan = keyframes`
+  0%, 100% {
+    transform: scaleX(0);
+    transform-origin: left;
   }
-  20% {
-    opacity: 1;
+  40%, 60% {
+    transform: scaleX(1);
+    transform-origin: left;
   }
-  100% {
-    opacity: 0;
-    transform: translate(45px, -25px) scale(0.3);
+  60.1%, 100% {
+    transform-origin: right;
   }
 `;
 
-// Keyframes for a subtle cart jiggle
-const cartJiggle = keyframes`
-  0%, 100% {
-    transform: translateX(0);
+// Keyframes for the checkmark drawing and fading
+const drawCheck = keyframes`
+  0%, 25% {
+    stroke-dashoffset: 48;
   }
   50% {
-    transform: translateX(-2px);
+    stroke-dashoffset: 0;
+  }
+  75%, 100% {
+    stroke-dashoffset: 0;
+    opacity: 0;
   }
 `;
+
 
 // --- Styled Components ---
 
@@ -40,53 +45,59 @@ const LoaderWrapper = styled.div`
   align-items: center;
 `;
 
-// Container for the cart and the animated items
-const AnimationContainer = styled.div`
-  position: relative;
+// Container for the scanning animation
+const ScannerContainer = styled.div`
   width: 80px;
   height: 80px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
 `;
 
-// The shopping cart icon
-const Cart = styled.div`
-  position: absolute;
-  bottom: 0;
-  left: 10px;
-  width: 60px;
-  height: 60px;
-  animation: ${cartJiggle} 1.5s ease-in-out infinite;
+// A single scanning line
+const ScanLine = styled.div`
+  width: 100%;
+  height: 4px;
+  background-color: #bdc3c7; // A soft grey color
+  margin: 4px 0;
+  border-radius: 2px;
+  transform: scaleX(0);
+  animation: ${scan} 2.5s linear infinite;
 
-  // SVG for the cart icon
-  svg {
-    width: 100%;
-    height: 100%;
-    fill: #34495e; // A nice, dark slate color
-  }
-`;
-
-// The items that fly into the cart
-const Item = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 15px;
-  height: 15px;
-  background-color: #2ecc71; // A vibrant green, suggesting success/addition
-  border-radius: 50%;
-  opacity: 0;
-  animation: ${addToCartAnimation} 1.5s ease-in-out infinite;
-
-  // Stagger the animation for each item
+  // Stagger the animation for each line
   &:nth-child(1) {
+    width: 60%;
     animation-delay: 0s;
   }
   &:nth-child(2) {
-    animation-delay: 0.25s;
-    background-color: #3498db; // A different color for variety
+    width: 100%;
+    animation-delay: 0.2s;
   }
   &:nth-child(3) {
-    animation-delay: 0.5s;
+    width: 80%;
+    animation-delay: 0.4s;
   }
+`;
+
+// The checkmark SVG that appears after the scan
+const Checkmark = styled.svg`
+  width: 30px;
+  height: 30px;
+  position: absolute;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+  stroke: #27ae60; // A professional green
+  stroke-width: 5;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+  fill: none;
+  stroke-dasharray: 48;
+  stroke-dashoffset: 48;
+  animation: ${drawCheck} 2.5s ease-in-out infinite;
+  animation-delay: 0.6s; // Starts after the lines have animated
 `;
 
 
@@ -95,20 +106,17 @@ const Item = styled.div`
 const Loader = ({ fullscreen }) => {
     return (
         <LoaderWrapper fullscreen={fullscreen}>
-            <AnimationContainer>
-                {/* The items to be animated */}
-                <Item />
-                <Item />
-                <Item />
+            <ScannerContainer>
+                {/* The scanning lines */}
+                <ScanLine />
+                <ScanLine />
+                <ScanLine />
 
-                {/* The cart icon */}
-                <Cart>
-                    {/* --- UPDATED SVG ICON --- */}
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                        <path d="M7 18c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2-.9-2-2-2zM1 2v2h2l3.6 7.59-1.35 2.45c-.16.28-.25.61-.25.96 0 1.1.9 2 2 2h12v-2H7.42c-.14 0-.25-.11-.25-.25l.03-.12.9-1.63h7.45c.75 0 1.41-.41 1.75-1.03l3.58-6.49c.08-.14.12-.31.12-.48 0-.55-.45-1-1-1H5.21l-.94-2H1zm16 16c-1.1 0-1.99.9-1.99 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z" />
-                    </svg>
-                </Cart>
-            </AnimationContainer>
+                {/* The checkmark icon */}
+                <Checkmark viewBox="0 0 52 52">
+                    <path d="M14.1 27.2l7.1 7.2 16.7-16.8" />
+                </Checkmark>
+            </ScannerContainer>
         </LoaderWrapper>
     );
 };
